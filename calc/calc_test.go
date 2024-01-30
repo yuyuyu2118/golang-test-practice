28002444e2f45486ea1595a92d3abc04ffba3b5f
yuyuyu2118 <yuyuyu2118@gmail.com>
2024-01-30 10:38:10 +0900

    refactor/calc-test: 不要なコメントを削除

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -4,16 +4,6 @@ import (
 	"testing"
 )
 
-// Test<関数名>(t *testing.T)
-// この関数は、go testコマンドで実行される
-// func TestMax(t *testing.T) {
-// 	got := Max(1, 2)
-// 	want := 2
-// 	if got != want {
-// 		t.Errorf("Max(1, 2) == %d, want %d", got, want)
-// 	}
-// }
-
 // テーブル駆動テスト
 func TestMax(t *testing.T) {
 	tests := []struct {
@@ -30,7 +20,7 @@ func TestMax(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Max(tt.x, tt.y); got != tt.want {
-				t.Errorf(("Max(%d, %d) == %d, want %d"), tt.x, tt.y, got, tt.want)
+				t.Errorf("Max(%d, %d) == %d, want %d", tt.x, tt.y, got, tt.want)
 			}
 		})
 	}
