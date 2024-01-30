42a00c845730601527e3f576920fa9282b056d4e
yuyuyu2118 <yuyuyu2118@gmail.com>
2024-01-30 10:28:05 +0900

    feature/calc-test: Max関数のテーブル駆動テスト追加

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -1,20 +1,37 @@
 package calc
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestMax(t *testing.T) {
-	got := Max(1, 2)
-	want := 2
-	if got != want {
-		t.Errorf("Max(1, 2) == %d, want %d", got, want)
-	}
-}
+// Test<関数名>(t *testing.T)
+// この関数は、go testコマンドで実行される
+// func TestMax(t *testing.T) {
+// 	got := Max(1, 2)
+// 	want := 2
+// 	if got != want {
+// 		t.Errorf("Max(1, 2) == %d, want %d", got, want)
+// 	}
+// }
 
-func ExampleMax() {
-	fmt.Println(Max(1, 2))
-	// Output:
-	// 2
+// テーブル駆動テスト
+func TestMax(t *testing.T) {
+	tests := []struct {
+		name string
+		x    int
+		y    int
+		want int
+	}{
+		{"xが大きい", 2, 1, 2},
+		{"yが大きい", 1, 3, 3},
+		{"xとyが同じ", 5, 5, 5},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Max(tt.x, tt.y); got != tt.want {
+				t.Errorf(("Max(%d, %d) == %d, want %d"), tt.x, tt.y, got, tt.want)
+			}
+		})
+	}
 }
