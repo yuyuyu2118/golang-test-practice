1960c2a4f31d37d641c6d74331be7ac57125fe98
yuyuyu2118 <yuyuyu2118@gmail.com>
2024-02-02 15:53:03 +0900

    feature/add-language-support: 言語サポートを追加

--- learn-go-with-tests/hello-world/hello.go
+++ learn-go-with-tests/hello-world/hello.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	prefix := englishHelloPrefix
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "Japanese"))
 }
